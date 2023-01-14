0a7022c1bd1aa49d1822ce09d32998751e069360
Fülöp Tibor <tibyy17@gmail.com>
2023-01-14 15:50:16 +0100

    Javascript file elkészűlt. Css fileban további utasítások elkészültek.

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-const order = document.getElementById("order");
+const result = document.getElementById(".result");
+
+window.addEventListener("load", async () => {
+
+    const res = await fetch("order.json");
+    const data = await res.json();
+
+    console.log(data);
+
+    let output = "";
+
+    data.map(order => {
+
+        output += `
+        
+        <div class="order">
+        
+        <div>
+        <img src='${order.kep}' alt="pizza" />
+        </div>
+        <h3>${order.pizza}</h3>
+        <hr>
+        <h4>Ár:${order.ar}</h4>
+        </div>
+        `;
+
+    })
+  
+    result.innerHTML = output;
+})
